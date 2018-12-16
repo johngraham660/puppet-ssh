60187ddd589924524d0cae3993547ce404a15dd7
John Graham <john.graham@virtua-enterprise.co.uk>
2018-12-16 17:28:36 +0000

    Checking in changes

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,16 @@
-# A description of what this class does
-#
-# @summary A short summary of the purpose of this class
-#
-# @example
-#   include ssh
+# ================================================
+# A class to manage the ssh service on Linux hosts
+# ================================================
+# @param ssh_service_enable [Boolean] Ensure the service is enabled in systemd
+# @param ssh_service_ensure [String] Ensure the service is running
+# @param ssh_config_x11forwarding [Boolean] Toggle whether X11 forwarding is allowed or not.
+# @param ssh_config_print_motd [Boolean] Toggle whether the MOTD gets printed at login
+# @param ssh_config_print_banner [Boolean] Toggle whether the banner page get presented at the login prompt.
+# @param ssh_config_banner_path [String] Location of the banner message
+# @param ssh_config_use_dns [Boolean] Should only be set to yes if your doing host bases authentication.
+# @param ssh_config_strictmodes [Boolean] Toggle whether ssh should check for file modes and ownership before accepting login requests
+# @param ssh_config_permitrootlogin [Boolean] Determine if direct root login is allowed or not
+
 class ssh (
 
   $ssh_service_enable         = $::ssh::params::ssh_service_enable,
